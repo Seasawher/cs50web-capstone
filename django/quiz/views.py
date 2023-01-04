62b849c01790d9b1d787c4ea5a1ea5c7cec4e03b
seasawher <tellagoodstory@icloud.com>
2023-01-04 13:35:39 +0000

    Feature ✨ You can see the preview of index.html

--- django/quiz/views.py
+++ django/quiz/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, HttpRequest
 
 # Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "quiz/index.html")

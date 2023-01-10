fd87190eeb47342355b38f4c3010e6ace56c7c55
seasawher <tellagoodstory@icloud.com>
2023-01-10 13:48:19 +0000

    Refactoring ♻️ Use React to display "Hello World"

---
+++ django/quiz/static/quiz/app.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <h1 className="text-3xl font-bold">Hello World!!</h1>
+    );
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App/>);

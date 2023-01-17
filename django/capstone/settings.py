9e91a1b992e3835962ec99850e6c804be19e5f77
seasawher <tellagoodstory@icloud.com>
2023-01-17 11:13:51 +0000

    Config 🛠️ set up heroicons for django

--- django/capstone/settings.py
+++ django/capstone/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "quiz",
     "rest_framework",
+    "heroicons",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

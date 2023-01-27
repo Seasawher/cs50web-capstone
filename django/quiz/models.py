9061ae85b4836f3b576684f1d27ba668afd02c1f
seasawher <tellagoodstory@icloud.com>
2023-01-27 11:45:22 +0000

    HotFix 🐞 Change textarea to text field

--- django/quiz/models.py
+++ django/quiz/models.py
@@ -47,10 +47,10 @@ class Submission(TimeStampedModel):
     quiz = models.ForeignKey(
         Quiz, on_delete=models.CASCADE, related_name="received_submissions"
     )
-    submitted_answer = models.TextField(null=True)
+    submitted_answer = models.CharField(max_length=30 ,null=True)
 
     @property
     def is_accepted(self) -> bool:
         """returns if the submission is accepted"""
-        
+
         pass

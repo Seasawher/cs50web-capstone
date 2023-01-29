35450b9274c0d918361ddeeaa1835629b9fbb2cf
seasawher <tellagoodstory@icloud.com>
2023-01-29 03:59:57 +0000

    Layout 🎨 make a mock of each quiz's state

--- django/quiz/static/quizList.jsx
+++ django/quiz/static/quizList.jsx
@@ -26,7 +26,7 @@ function App() {
                         key={quiz['id']}
                         id={quiz['id']}
                         author={quiz['user']['username']}
-                        state="solved"
+                        state="attempted"
                         title={quiz['title']}
                         timestamp={formatTime(quiz['created_at'])}
                         star={quiz['gained_stars'].length}
@@ -37,16 +37,40 @@ function App() {
     }
 }
 
+function State({ state }) {
+    if (state == 'todo') {
+        return (
+            <div className="text-slate-400 inline mr-2">
+                <span className="inline mr-1">-</span>
+                {state}
+            </div>
+        );
+    }
+    if (state == 'attempted') {
+        return (
+            <div className="text-amber-300 inline mr-2">
+                <ExclamationCircleIcon className="inline mr-1 w-4 h-4" />
+                {state}
+            </div>
+        );
+    }
+    if (state == 'solved') {
+        return (
+            <div className="text-emerald-300 inline mr-2">
+                <CheckCircleIcon className="inline w-4 h-4 mr-1" />
+                {state}
+            </div>
+        );
+    }
+}
+
 function Quiz({ id, author, state, title, timestamp, star }) {
     const current_url = location.href;
     const target_url = `${current_url}quiz/${id}`;
     return (
         <div className="pb-6 pt-3 px-8 mb-3 bg-slate-700 rounded-lg">
-            <div class="mb-4">
-                <div className="text-emerald-300 inline mr-2">
-                    <CheckCircleIcon className="inline w-4 h-4 mr-1" />
-                    {state}
-                </div>
+            <div className="mb-4">
+                <State state={state} />
                 <div className="inline">
                     <StarIcon className="inline w-4 h-4 mr-1" />
                     {star}

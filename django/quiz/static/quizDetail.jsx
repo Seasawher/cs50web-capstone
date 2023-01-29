6c30ab351ab61782da4a2f46e43e334094bada08
seasawher <tellagoodstory@icloud.com>
2023-01-29 15:05:12 +0000

    Construction 🚧 display content, title...

--- django/quiz/static/quizDetail.jsx
+++ django/quiz/static/quizDetail.jsx
@@ -1,11 +1,11 @@
-function QuizDetail({ id }) {
+function QuizDetail({ id, title, content }) {
     return (
         <div className="py-6 px-8 mb-3 bg-slate-700 rounded-lg">
             <h2 className="text-2xl mb-4 font-bold text-sky-300">
-                Title
+                {title}
                 <span className="ml-3 text-slate-400">#{ id }</span>
             </h2>
-            <div className="[&>p]:mt-7 mb-16">Content</div>
+            <div className="[&>p]:mt-7 mb-16">{content}</div>
             <form action="" method="post">
                 <div className="flex">
                     <button type="button" className="mx-4"></button>

6f0e891949f9fa54cfd15c2259ba3ca750297ae2
Seasawher <tellagoodstory@icloud.com>
2023-02-11 09:18:32 +0000

    Layout 🎨 prepare `login.tsx` and `logout.tsx`

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
@@ -6,14 +7,15 @@ export default function Navbar() {
       <nav className='bg-slate-800'>
         <div className='flex flex-row top-0 left-0 p-4'>
           <div className='basis-3/6'>
-            <a className="text-xl font-semibold text-sky-300 hover:text-sky-500"
+            <Link className="text-xl font-semibold text-sky-300 hover:text-sky-500"
               href="/">
               Capstone
-            </a>
+            </Link>
           </div>
           <div className="basis-3/6 text-right text-l">
             <span className="hover:underline hover:text-sky-400 mx-3">
-              <a href="./logout">Log Out</a>
+              <Link href="./logout">Log Out</Link>
+              <Link href="./login">Log In</Link>
             </span>
           </div>
         </div>

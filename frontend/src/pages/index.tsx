cb44a3d555f3dd66585de232343206ebf8a396e9
Seasawher <tellagoodstory@icloud.com>
2023-02-11 10:01:47 +0000

    Config 🛠️ format tailwindCSS class attributes in `tsx`

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -10,9 +10,7 @@ export default function Home() {
       <Head>
         <title>Capstone</title>
       </Head>
-        <h1 className="text-3xl font-bold underline text-rose-300">
-          Hello NextJS and TailwindCSS!!
-        </h1>
+      <h1 className="text-3xl font-bold text-rose-300 underline">Hello NextJS and TailwindCSS!!</h1>
     </>
-  )
+  );
 }

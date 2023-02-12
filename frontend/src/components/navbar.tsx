d24d345ef918b1888768c18c6b5e709ee7a04e70
Seasawher <tellagoodstory@icloud.com>
2023-02-12 08:02:24 +0000

    Layout 🚧 login and register page

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,25 +1,24 @@
-import React from "react";
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
 export default function Navbar() {
   return (
     <>
-      <nav className='bg-slate-800'>
-        <div className='flex flex-row top-0 left-0 p-4'>
-          <div className='basis-3/6'>
-            <Link className="text-xl font-semibold text-sky-300 hover:text-sky-500"
-              href="/">
+      <nav className="bg-slate-800">
+        <div className="top-0 left-0 flex flex-row p-4">
+          <div className="basis-3/6">
+            <Link className="text-xl font-semibold text-sky-300 hover:text-sky-500" href="/">
               Capstone
             </Link>
           </div>
-          <div className="basis-3/6 text-right text-l">
-            <span className="hover:underline hover:text-sky-400 mx-3">
-              <Link href="./logout">Log Out</Link>
+          <div className="text-l basis-3/6 text-right">
+            <span className="mx-3 hover:text-sky-400 hover:underline">
+              <Link href="./register">Log Out</Link>
               <Link href="./login">Log In</Link>
             </span>
           </div>
         </div>
       </nav>
     </>
-  )
+  );
 }
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -6,18 +6,15 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Login</title>
+        <title>Register</title>
       </Head>
       <section className="mx-auto max-w-xl">
-        <h1 className="mb-6 text-3xl font-bold">Login</h1>
+        <h1 className="mb-6 text-3xl font-bold">Register</h1>
         <div className="mb-3 rounded-lg bg-slate-800 py-6 px-8"></div>
         <div className="mb-3 rounded-lg bg-slate-800 py-6 px-8">
-          Don't have an account?
-          <Link
-            className="text-sky-200 hover:text-sky-400 hover:underline"
-            href="./register"
-          >
-            Register here.
+          Already have an account?{' '}
+          <Link className="text-sky-200 hover:text-sky-400 hover:underline" href="./login">
+            Log In here.
           </Link>
         </div>
       </section>

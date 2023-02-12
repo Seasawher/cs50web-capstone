d7497361e05fd5304a443e6bbb86339d814d60ac
Seasawher <tellagoodstory@icloud.com>
2023-02-12 08:22:49 +0000

    Layout 🎨 restore a mock of login input form

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -10,13 +10,37 @@ export default function Home() {
       </Head>
       <section className="mx-auto max-w-xl">
         <h1 className="mb-6 text-3xl font-bold">Login</h1>
-        <div className="mb-3 rounded-lg bg-slate-800 py-6 px-8"></div>
         <div className="mb-3 rounded-lg bg-slate-800 py-6 px-8">
-          Don't have an account?
-          <Link
-            className="text-sky-200 hover:text-sky-400 hover:underline"
-            href="./register"
-          >
+          <form>
+            <div className="mb-6">
+              <label htmlFor="id_username" className="mb-3 block text-sm font-medium">
+                Username
+              </label>
+              <input
+                type="text"
+                id="id_username"
+                placeholder='username'
+                className="w-full rounded-lg
+                        bg-gray-600 px-3
+                        py-1 placeholder-gray-400 focus:caret-blue-500 focus:outline focus:outline-2 focus:outline-blue-500"
+              ></input>
+            </div>
+            <div className="mb-6">
+              <label htmlFor="id_password" className="mb-3 block text-sm font-medium">
+                Password
+              </label>
+              <input
+                type="password"
+                id="id_password"
+                placeholder='password'
+                className="w-full rounded-lg bg-gray-600 px-3 py-1 placeholder-gray-400 focus:caret-blue-500 focus:outline focus:outline-2 focus:outline-blue-500"
+              ></input>
+            </div>
+          </form>
+        </div>
+        <div className="mb-3 rounded-lg bg-slate-800 py-6 px-8">
+          Don't have an account?{' '}
+          <Link className="text-sky-200 hover:text-sky-400 hover:underline" href="./register">
             Register here.
           </Link>
         </div>

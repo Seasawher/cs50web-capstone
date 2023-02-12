9546d73f338d7e23c54ced288b84da6a8a4c64db
Seasawher <tellagoodstory@icloud.com>
2023-02-12 08:24:45 +0000

    Layout 🎨 restore a login button

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -19,7 +19,7 @@ export default function Home() {
               <input
                 type="text"
                 id="id_username"
-                placeholder='username'
+                placeholder="username"
                 className="w-full rounded-lg
                         bg-gray-600 px-3
                         py-1 placeholder-gray-400 focus:caret-blue-500 focus:outline focus:outline-2 focus:outline-blue-500"
@@ -32,10 +32,16 @@ export default function Home() {
               <input
                 type="password"
                 id="id_password"
-                placeholder='password'
+                placeholder="password"
                 className="w-full rounded-lg bg-gray-600 px-3 py-1 placeholder-gray-400 focus:caret-blue-500 focus:outline focus:outline-2 focus:outline-blue-500"
               ></input>
             </div>
+            <button
+              type="submit"
+              className="my-3 rounded-lg bg-cyan-600 px-5 py-1.5 font-medium text-white hover:bg-cyan-700 focus:outline focus:outline-2 focus:outline-blue-500"
+            >
+              Login
+            </button>
           </form>
         </div>
         <div className="mb-3 rounded-lg bg-slate-800 py-6 px-8">
